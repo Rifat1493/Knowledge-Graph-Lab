b00c87545bbc182cb4f556f3abf3652a08685dc9
spencerlii <spencer850922@gmail.com>
2022-05-22 21:10:45 +0200

    Add files via upload

---
+++ src/main/java/publicationOntology/App.java
@@ -0,0 +1,10 @@
+package publicationOntology;
+
+import publicationOntology.abox.Creator;
+
+public class App 
+{
+    public static void main(String[] args) throws Exception {
+			Creator.createALL();
+    }
+}
